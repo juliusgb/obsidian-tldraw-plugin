6fb3765786df71290b3735fa8802801614d3ef20
Julius Gamanyi <juliusgb@users.noreply.github.com>
2022-09-05 05:00:16 +0200

    refactor: extract to methods

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,8 @@ export default class TldrawPlugin extends Plugin {
 		addIcon(ICON_NAME, TLDRAW_ICON);
 
 		await this.loadSettings();
+		// adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new TldrawSettingTab(this.app, this));
 
 		// register custom view with the plugin
 		this.registerView(
@@ -43,28 +45,9 @@ export default class TldrawPlugin extends Plugin {
 		// Register the extensions you want the view to handle.
 		this.registerExtensions(["tldraw"], VIEW_TYPE_TLDRAW_EMBED);
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon(TLDRAW_ICON, 'New Tldraw drawing', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('TLdraw clicked!');
-			this.activateView();
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('in Tldraw');
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new TldrawSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerCommands();
+		this.registerEventListeners();
+		this.setStatusBar();
 	}
 
 	onunload() {
@@ -78,6 +61,34 @@ export default class TldrawPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	private registerCommands() {
+		// TODO: handle i18n (internationalization and localization)
+
+		// This creates an icon in the left ribbon.
+		this.addRibbonIcon(TLDRAW_ICON, 'New Tldraw drawing', (evt: MouseEvent) => {
+			// Called when the user clicks the icon.
+			new Notice('TLdraw clicked!');
+			this.activateView();
+		});
+	}
+
+	private registerEventListeners() {
+		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// Using this function will automatically remove the event listener when this plugin is disabled.
+		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+			console.log('click', evt);
+		});
+
+		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+
+	private setStatusBar() {
+		// adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		const statusBarItemEl = this.addStatusBarItem();
+		statusBarItemEl.setText('in Tldraw');
 	}
 
 	/**
