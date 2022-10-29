c63b06a1566a51170d9253b01a48ba2d26a3dc86
Julius Gamanyi <juliusgb@users.noreply.github.com>
2022-10-29 08:18:58 +0200

    docs: update field's description

--- src/TLdrawData.ts
+++ src/TLdrawData.ts
@@ -19,19 +19,11 @@ export class TLdrawData {
 	 * <br/>
 	 * When user triggers the plugin to create a tldraw drawing:
 	 * the plugin takes the json strings this member field
-	 * 	- json string -> tldrawDataInTDFile -> TDFile
+	 * 	- json string -> tldrawDataFile -> TDFile
 	 * <br/>
-	 * After user makes changes in Tldraw,
-	 *
+	 * After user makes changes in Tldraw, this file is updated
 	 */
-
-	// tldraw specific types
-	// initial file loaded into tldraw
-	// json string -> tldrawDataInTDFile -> TDFile
-	// same field is used after user makes changes in tldraw
 	public tldrawDataFile: TDFile;
-	// after file is modified in tldraw and tldraw sends changes back
-	// tldraw -> tldrawDataOutTDFile -> obsidian
 
 
 	public async loadLegacyData(data: string, file: TFile): Promise<boolean> {
@@ -57,7 +49,8 @@ export class TLdrawData {
 			new Notice('The file looks corrupted!');
 		}
 
-		// debug({where:"TLdrawData.loadLegacyData",file:this.file.name, data:this.tldrawJson, after:"loadLegacyData"});
+		// debug({where:"TLdrawData.loadLegacyData", file:this.file.name, mData: data, dataJson: this.tldrawJson,
+		// dataAsTDFile: this.tldrawDataFile, });
 
 		return true;
 
